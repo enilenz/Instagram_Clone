30089a95e1fb95d8b28f99f0a0b2b7a033f353b1
enilenz <eni.odubawo@gmail.com>
2020-02-17 00:05:15 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android/instagramclone/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.android.instagramclone;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int WRITE_STORAGE_REQUEST_CODE = 0;
+    private static int READ_STORAGE_REQUEST_CODE = 1;
+    private static int CAMERA_REQUEST_CODE = 2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(this , Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, WRITE_STORAGE_REQUEST_CODE);
+        }
+
+        if (ContextCompat.checkSelfPermission(this , Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, READ_STORAGE_REQUEST_CODE);
+        }
+
+        if (ContextCompat.checkSelfPermission(this , Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAMERA_REQUEST_CODE);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/android/instagramclone/SplashScreen.java
@@ -0,0 +1,41 @@
+package com.example.android.instagramclone;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    int SPLASH_TIMEOUT = 3000;
+
+    ImageView imageView;
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        imageView = findViewById(R.id.instagramTitleView);
+        textView = findViewById(R.id.textView);
+
+//        imageView.animate().alpha(10).setDuration(4000);
+        textView.animate().alpha(10).setDuration(6000);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent splashIntent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(splashIntent);
+                finish();
+
+            }
+        } , SPLASH_TIMEOUT);
+
+    }
+}
